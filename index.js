6107c6d51a92bd5e70c3e047ea5dee91846a6592
JPChoyon <kotjpckhan@gmail.com>
2025-08-15 14:14:27 +0600

    2nd problem solving done

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ function getMaleNames(arr) {
 }
 
 console.log(getMaleNames(people));  // Output: [ 'Jhankar', 'Hamid', 'hasnain',]
+
+
+const books = [
+  { title: "Node js", author: "Jhankar", year: 2000 },
+  { title: "Hate Khori", author: "Jhankar", year: 2010 }
+];
+
+function getBookTitles(arr) {
+  return arr.map(book => book.title);
+}
+
+console.log(getBookTitles(books)); // Output: [ 'Node js', 'Hate Khori' ]
